970d79cb5adcbfd144b516d0ee42c5b21c2e2ba1
Saad Alsaad <1185480@student.birzeit.edu>
2022-03-25 20:06:40 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -44,12 +44,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blango_auth',
     'blog',
     'crispy_forms',
     'crispy_bootstrap5',
     'debug_toolbar',
 ]
 
+AUTH_USER_MODEL = "blango_auth.User"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
