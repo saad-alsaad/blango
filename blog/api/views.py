ade6b9ae432973daabb4167247d92d1b505033f5
Saad Alsaad <1185480@student.birzeit.edu>
2022-04-08 14:34:49 +0000

    Finish permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -2,14 +2,17 @@ from rest_framework import generics
 from rest_framework.authentication import SessionAuthentication
 from blog.api.serializers import PostSerializer
 from blog.models import Post
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     authentication_classes = [SessionAuthentication]
+    permission_classes = [AuthorModifyOrReadOnly]
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
