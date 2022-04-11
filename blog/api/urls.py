f5f64f6e1bd4c9eaec98f8ceb7f107ac0ceac255
Saad Alsaad <1185480@student.birzeit.edu>
2022-04-11 21:50:07 +0000

    Finish viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
-from blog.api.views import PostList, PostDetail, UserDetail
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
 from rest_framework.authtoken import views
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
+from rest_framework.routers import DefaultRouter
 
+
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -18,8 +23,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+    # path("posts/", PostList.as_view(), name="api_post_list"),
+    # path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 
@@ -45,3 +50,8 @@ urlpatterns += [
     ),
 ]
 
+urlpatterns += [
+    path("auth/", include("rest_framework.urls")),
+    # ... other patterns omitted
+    path("", include(router.urls)),
+]
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,10 +1,37 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.authentication import SessionAuthentication
-from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
-from blog.models import Post
+from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer, TagSerializer
+from blog.models import Post, Tag
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 from blango_auth.models import User
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    @action(methods=["get"], detail=True, name="Posts with the Tag")
+    def posts(self, request, pk=None):
+        tag = self.get_object()
+        post_serializer = PostSerializer(
+            tag.posts, many=True, context={"request": request}
+        )
+        return Response(post_serializer.data)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
+    queryset = Post.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ("list", "create"):
+            return PostSerializer
+        return PostDetailSerializer
+
+"""
+PostList and PostDetail views are being replaced with PostViewSet
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -17,7 +44,7 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
-
+"""
 
 class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
