b9e07a8deedba97e84e541adf2cbdccd778012eb
Saad Alsaad <1185480@student.birzeit.edu>
2022-04-23 22:40:10 +0000

    Finish filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -54,4 +54,9 @@ urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     # ... other patterns omitted
     path("", include(router.urls)),
+    path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ),
 ]
